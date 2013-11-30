d96fdc39ed2f330ce66e251737400e28352d6c93
Cameron <cammy.wan.kenobi@gmail.com>
2013-11-30 19:22:18 +0000

    Initial import of the DFP.js library.

---
+++ README.md
@@ -0,0 +1,140 @@
+# DFP.js
+
+Built from hard knocks and frustration over several years of trying to get it to work in various types of website and scanning through Google's GPT documentation.
+
+The DFP.js to library aim is simplify the implementation of DFP and provide some much needed flexibility when advertising managers decide to have random positions or random sizes!
+
+# Dependencies
+
+This JavaScript only has a single hard dependency; jQuery.  It is not a jQuery plugin but makes use of several jQuery methods to simplify the implementation.
+
+# How To Use DFP.js
+
+## Installation
+
+Download the DFP.js or DFP.min.js from this repository and then include it under your jQuery include;
+
+```html
+<script src="/path/to/jquery.js"></script>
+<script src="/path/to/dfp.js"></script>
+```
+
+## Basic Setup
+
+Once you have included the DFP.js file in the header, then the next step is to prepare the main call which calls DFP to include the relevant scripts.  It will also scan the main HTML document to find the adverts to be served.
+
+### HTML Ad Slots
+
+Place a piece of HTML like the below where you wish the adverts to appear on the page.
+
+```html
+<div rel="advert" data-sizes="728x90"></div>
+<div rel="advert" data-sizes="300x250"></div>
+```
+
+### JavaScript Execute
+
+And make sure the following JavaScript is either in the head or above the `</body>` tag on the page.
+
+```javascript
+( function ( $ ) {
+    $( document ).ready( function () {
+    	/**
+    	 * Must-Have settings - this part sets the basic network and Zone for the overall page.
+    	 */
+        dfp.collapsable = true;
+        dfp.network = '/NetworkCode/UnitName';
+        dfp.zone = 'home';
+
+        /**
+         * Execute DFP cycle - both methods need to be called to get the ads on the page to serve.
+         */
+        dfp.enable();
+        dfp.display();
+    } );
+} )( jQuery );
+```
+
+And you're done!
+
+## Additional Implementation Information
+
+Whilst the above will get you up and running in no time, the below will provide you with the additional information you'll need to get the ads doing ever increasing numbers of fancy things.
+
+### Companion Ads
+
+Need that leaderboard and MPU to serve a companion set?  That's easily done by adding the `data-companion="yes"` attribute to the `<div rel="advert"></div>`.
+
+```html
+<div rel="advert" data-companion="yes" data-sizes="728x90"></div>
+<div rel="advert" data-companion="yes" data-sizes="300x250"></div>
+```
+
+### Multiple Sizes
+
+If you wish to serve an MPU or double MPU in the same place, just add specify the sizes separated by a comma.
+
+```html
+<div rel="advert" data-sizes="300x250,300x600"></div>
+``` 
+
+### Set Targeting for All Slots
+
+To set targeting for all slots, then add the entries to `dfp.targeting_all` array prior to calling the `dfp.enable()` method.
+
+```javascript
+( function ( $ ) {
+    $( document ).ready( function () {
+        dfp.collapsable = true;
+        dfp.network = '/NetworkCode/UnitName';
+        dfp.zone = 'home';
+
+        /**
+         * Set some additional targeting for the adverts.
+         */
+        dfp.targeting_all.push( { "key" : "interests", "value" : ["sports", "music", "movies"] } );
+
+        dfp.enable();
+        dfp.display();
+    } );
+} )( jQuery );
+```
+
+### Set Targeting for a Single Slot
+
+Best example is if you have a page which contains two MPU adverts; one at the top and one at the bottom.  For obvious reasons, the top one will be most valuable especially if it's very long page and therefore advertisers will need to be able target it by itself.
+
+That's achieved by adding the `data-targeting="pos=top"` attribute to the `<div rel="advert"></div>`.
+
+```html
+<div rel="advert" data-sizes="300x250" data-targeting="pos=top"></div>
+```
+
+Multiple targeting options can be specified by separating them with a comma.
+
+```html
+<div rel="advert" data-sizes="300x250" data-targeting="pos=top,gender=male,age=20-30"></div>
+<div rel="advert" data-sizes="300x250" data-targeting="x=right,y=top"></div>
+```
+
+### Dynamic Ads
+
+If your site performs AJAX, perhaps using some PJAX technique, and HTML contains new slots which need to be displayed, you can use `dfp.cycle()` method.
+
+```javascript
+$.ajax( {
+	url : "test.html",
+	cache : false
+} ).done( function( html ) {
+	$( "#results" ).append( html );
+	dfp.cycle();
+} );
+```
+
+## Doesn't DFP require IDs for those divs?
+
+Yes.  But the library will dynamically set a unique ID if the `<div rel="advert"></div>` element does not have one already.  So no need to worry about it!
+
+## License
+
+DFP.js is licensed under the MIT open source license.
---
+++ dfp.js
@@ -0,0 +1,169 @@
+/**
+ * DFP.js 1.0.0
+ * 
+ * Copyright (c) 2013 Cameron Terry
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+
+( function ( $ ) {
+    /**
+     * Quote from DFP support; "boiler-plate that asynchronously loads the
+     * GPT JavaScript library used by DFP, using SSL/HTTPS if necessary".
+     */
+    var gads = document.createElement( 'script' );
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = ( useSSL ? 'https:' : 'http:' ) +
+                '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName( 'script' )[0];
+    node.parentNode.insertBefore( gads, node );
+
+    /**
+     * DFP global management object.
+     */
+    var dfp = {
+        collapsable : false,
+        ad_slots : [],
+        network : '',
+        targeting_all : [],
+        zone : '',
+        defineSlots : function () {
+            $( 'div[rel="advert"][data-ad-complete!="yes"]' ).each( function () {
+                var $this = $( this );
+                var id = dfp.getID( $this );
+                var advert = googletag.defineSlot( dfp.network + dfp.zone, dfp.getSizes( $this.data( 'sizes' ) ), id );
+                
+                dfp.setTargetingSingle( $this.data( 'targeting' ), advert );
+                dfp.setCompanionSingle( $this.data( 'companion' ), advert );
+
+                advert.addService( googletag.pubads() );
+                dfp.ad_slots[id] = advert;
+            });
+        },
+        getID : function ( el ) {
+            if ( el.attr( 'id' ) ) {
+                return el.attr( 'id' );
+            }
+
+            var NewID = 'div-gpt-ad-' + Math.random().toString( 36 ).substr( 2, 9 );
+            el.prop( 'id', NewID );
+
+            return NewID;
+        },
+        getSizes : function ( size_string ) {
+            return $.map( size_string.split( ',' ), function ( el, i ) {
+                var wh = el.split( 'x' );
+                return [[Number( wh[0] ), Number( wh[1] )]];
+            } );
+        },
+        setCompanionSingle : function ( companion_definition, advert ) {
+            if ( companion_definition && companion_definition === 'yes' ) {
+                advert.addService( googletag.companionAds() );
+            }
+        },
+        setTargetingAll : function () {
+            for ( var i = 0; i < dfp.targeting_all.length; ++i ) {
+                googletag.pubads().setTargeting( dfp.targeting_all[i].key, dfp.targeting_all[i].value );
+            }
+        },
+        setTargetingSingle : function ( target_definition, advert ) {
+            if ( target_definition ) {
+                var targets_array = target_definition.split( '|' );
+
+                for ( var i = 0; i < targets_array.length; ++i ) {
+                    var key_value = targets_array[i].split( '=' );
+
+                    /**
+                     * Make the call to setTargeting on the GPT adSlot object.
+                     */
+                    advert.setTargeting( key_value[0], ( key_value[1].indexOf( ',' ) === -1 ? key_value[1] : key_value[1].split( ',' ) ) );
+                }
+            }
+        },
+        cycle : function() {
+            dfp.defineSlots();
+
+            var ad_slot_ids = dfp.display();
+
+            for ( var i = 0; i < ad_slot_ids.length; ++i ) {
+                dfp.refresh( ad_slot_ids[i] );
+            }
+        },
+        display : function () {
+            var return_val = [];
+
+            $( 'div[rel="advert"][data-ad-complete!="yes"]' ).each( function () {
+                var $this = $( this );
+                var ad_id = $this.attr( 'id' );
+
+                googletag.cmd.push( function () {
+                    googletag.display( ad_id );
+                } );
+
+                $this.attr( 'data-ad-complete', 'yes' );
+                return_val.push( ad_id );
+            } );
+
+            return return_val;
+        },
+        enable : function () {
+            googletag.cmd.push( function () {
+                dfp.defineSlots();
+                dfp.setTargetingAll();
+                
+                googletag.pubads().enableAsyncRendering();
+
+                /**
+                 * Whilst it does not state it anyway (that I can find), "Single Request"
+                 * is a pre-requisite for Companion Ads.
+                 */
+                googletag.pubads().enableSingleRequest();
+                
+                /**
+                 * If the ad tags have elements around them, then it's best to collapse
+                 * the ad tag so that the rest of the elements move into it's place.  This
+                 * is instead of leaving a "hole" on the page.
+                 */
+                googletag.pubads().collapseEmptyDivs( dfp.collapsable );
+                googletag.enableServices();
+
+                dfp.display();
+            } );
+        },
+        refresh : function ( id ) {
+            googletag.pubads().refresh( [dfp.ad_slots[id]] );
+        },
+        refreshAll : function () {
+            googletag.pubads().refresh();
+        },
+        takeover : function ( data ) {
+            var $link = $( '<a class="takeover" href="' + data.url + '" target="_blank"></a>' );
+            $link.css( 'background-image', 'url(' + data.image + ')' );
+
+            $( 'body' ).prepend( $link );
+        }
+    };
+
+    window.dfp = dfp;
+} )( jQuery );
