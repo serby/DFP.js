5a719624969eb2d5fca557c307edf8f3bc9726a7
Cameron Terry <cammy.wan.kenobi@gmail.com>
2015-09-30 15:16:20 +0100

    Updated the copyright year to 2015

--- dfp.js
+++ dfp.js
@@ -1,7 +1,7 @@
 /**
  * DFP.js 1.4.0
  * 
- * Copyright (c) 2013 Cameron Terry
+ * Copyright (c) 2015 Cameron Terry
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
